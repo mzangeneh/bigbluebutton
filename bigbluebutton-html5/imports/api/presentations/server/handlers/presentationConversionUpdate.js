8a1259961f4cfdd66d5ecc2d5798879c49189281
mzangeneh <mojtaba.zangeneh@gmail.com>
2020-06-03 15:35:42 +0430

    zip mime type added

--- bigbluebutton-html5/imports/api/presentations/server/handlers/presentationConversionUpdate.js
+++ bigbluebutton-html5/imports/api/presentations/server/handlers/presentationConversionUpdate.js
@@ -40,6 +40,7 @@ export default function handlePresentationConversionUpdate({ body }, meetingId) 
   switch (status) {
     case UNSUPPORTED_DOCUMENT_KEY:
     case SUPPORTED_DOCUMENT_KEY:
+      statusModifier['conversion.status'] = SUPPORTED_DOCUMENT_KEY;
       statusModifier.id = presentationId;
       statusModifier.name = presentationName;
       break;
