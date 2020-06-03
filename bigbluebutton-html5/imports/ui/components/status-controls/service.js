f567d8e5c3f02d56a3b9dfaba4d930576919c7ea
mzangeneh <mojtaba.zangeneh@gmail.com>
2020-06-03 11:30:56 +0430

    service added

--- bigbluebutton-html5/imports/ui/components/status-controls/service.js
+++ bigbluebutton-html5/imports/ui/components/status-controls/service.js
@@ -2,6 +2,8 @@ import Users from '/imports/api/users';
 import Auth from '/imports/ui/services/auth';
 import Meetings from '/imports/api/meetings';
 import logger from '/imports/startup/client/logger';
+import { EMOJI_STATUSES } from '/imports/utils/statuses';
+import { makeCall } from '/imports/ui/services/api';
 
 const init = (messages, intl) => {
   
@@ -11,9 +13,16 @@ const currentUser = () => {
   return Users.findOne({ userId: Auth.userID }, { fields: { approved: 1, emoji: 1 } });
 };
 
+const setEmojiStatus = (userId, emoji) => {
+  const statusAvailable = (Object.keys(EMOJI_STATUSES).includes(emoji));
 
+  return statusAvailable
+    ? makeCall('setEmojiStatus', Auth.userID, emoji)
+    : makeCall('setEmojiStatus', userId, 'none');
+};
 
 export default {
   init,
+  setEmojiStatus,
   currentUser
 };
