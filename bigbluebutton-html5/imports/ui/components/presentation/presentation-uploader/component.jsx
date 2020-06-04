7b34cd774a17bdc0b6e6e097da12a7e805125fad
mzangeneh <mojtaba.zangeneh@gmail.com>
2020-06-04 15:48:52 +0430

    upload conversion error

--- bigbluebutton-html5/imports/ui/components/presentation/presentation-uploader/component.jsx
+++ bigbluebutton-html5/imports/ui/components/presentation/presentation-uploader/component.jsx
@@ -722,7 +722,7 @@ class PresentationUploader extends Component {
 
     let awaitingConversion = false;
     presentations.map((presentation) => {
-      if (!presentation.conversion.done) awaitingConversion = true;
+      awaitingConversion = !presentation.conversion.done && !(presentation.filename.search('.zip') >= 0 || presentation.filename.search('.ZIP') >= 0);
       return null;
     });
 
